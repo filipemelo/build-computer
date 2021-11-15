77131bf693e51c85c22ff3acc5724a0d02883939
filipemelo <filipemelo.br@gmail.com>
2021-11-15 18:08:00 -0300

    Multiply R0 and R1 algorithm.

--- nand2tetris/nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/nand2tetris/projects/04/mult/Mult.asm
@@ -8,5 +8,41 @@
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+// R2 = R0 * R1
+@R0 // D = R0
+D=M
+@ZERO 
+D;JEQ // D == 0 JMP TO ZERO, because D = R0
 
-// Put your code here.
+@R1 // D = R1
+D=M
+@REPEAT // REPEAT = R1, because D = R1
+M=D
+@ZERO // D == 0 JMP TO ZERO, because D = R1
+D;JEQ
+
+@R2 // R2 = 0, cleans RAM[2] memory
+M=0
+
+(LOOP) // R0 AND R1, both greater than 0
+    @R0 // D = R0
+    D=M
+
+    @R2 // R2 = R0 + R2
+    M=D+M
+
+    @REPEAT // REPEAT--
+    M=M-1
+    D=M // D = REPEAT
+    @LOOP // REPEAT > 0 JMP TO LOOP
+    D;JGT 
+    @END // JMP TO END
+    0;JMP
+ 
+(ZERO) // R0 OR R1, one of them is equal to 0
+    @R2
+    M=0
+
+(END) // INFINITY JUMP TO NOT ACCESS NOT UNAUTHORIZED AREA
+    @END
+    0; JMP
